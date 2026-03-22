f22153d11b81220a3c3a9296c3a3791ebdf70790
Stefan <stefanseifeddine@gmail.com>
2026-03-22 12:02:43 +0000

    Add authentication routes to the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/auth/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Inventory Reminder API Running");
