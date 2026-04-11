dad297b7b635af5e5fef84e93ae51df3102f941f
Stefan <stefanseifeddine@gmail.com>
2026-04-11 18:49:59 +0100

    Add service worker event listeners for install and push

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,23 @@
 self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("install", (event) => {
     console.log("Service Worker installing...");
 });
 
 self.addEventListener("fetch", (event) => {
     // basic pass-through for now
+});
+
+self.addEventListener("push", function (event) {
+    const data = event.data.json();
+
+    self.registration.showNotification(data.title, {
+        body: data.body,
+    });
 });
