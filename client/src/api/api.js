3905439af9b1f6eb7ca79c7cc5efd9a15f791a59
Stefan <stefanseifeddine@gmail.com>
2026-04-25 17:13:54 +0100

    Feat: update API base URL to include '/api'

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://inventory-reminder-backend.onrender.com",
+    baseURL: "https://inventory-reminder-backend.onrender.com/api",
 });
 
 // Attach token automatically
